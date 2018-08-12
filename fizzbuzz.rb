9889a682602ac9aaec39f26124b5bea45b622f2a
Rachel Grier <rgrier104@gmail.com>
2018-08-12 22:21:06 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,11 +3,11 @@
 def fizzbuzz(int)
   if (int % 3 == 0)  && (int % 5 == 0)
     return "FizzBuzz"
-  elseif (int % 3 == 0)
+  end
+  if (int % 3 == 0)
     return "Fizz"
-  elseif (int % 5 == 0)
+  end
+  if (int % 5 == 0)
     return "Buzz"
-  else
-    return 
   end
 end
