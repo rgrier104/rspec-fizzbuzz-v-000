92fa77591b137a8d230a2bb78ef2b1c4df86383c
Rachel Grier <rgrier104@gmail.com>
2018-08-12 22:18:47 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,10 +2,12 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
   if (int % 3 == 0)  && (int % 5 == 0)
-    puts "FizzBuzz"
+    return "FizzBuzz"
   elseif (int % 3 == 0)
-    puts "Fizz"
+    return "Fizz"
   elseif (int % 5 == 0)
-    puts "Buzz"
+    return "Buzz"
+  else
+    return 
   end
 end
